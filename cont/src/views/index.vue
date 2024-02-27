20bbc96d06dcbd76adaf63f701c63b7fa33fafe2
mm333mm <a20miqmorcho@inspedralbes.cat>
2024-02-27 13:24:41 +0100

    asd

---
+++ cont/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Butons</h1>
+        <span v-for="num in 10">
+            <button @click="click(num)">{{ num }}</button>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            click(num) {
+                console.log(num)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
