e9ee660bb75f3fe491f5dc696c8bb3d606d13119
mm333mm <a20miqmorcho@inspedralbes.cat>
2024-02-28 09:43:31 +0100

    dd

--- cont/src/views/index.vue
+++ cont/src/views/index.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <h1>Butons atyttytgtyga</h1>
-        <span v-for="num in 10">
-            <button @click="click(num)">{{ num }}</button>
-        </span>
-    </div>
+  <div>
+    <h1>Butons atyttytgtyga</h1>
+    <span v-for="num in 10">
+      <button @click="click(num)">{{ num }}</button>
+    </span>
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            click(num) {
-                console.log(num)
-            }
-        }
+export default {
+  methods: {
+    click(num) {
+      console.log(num)
     }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
